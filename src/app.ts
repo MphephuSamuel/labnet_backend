15dc9d5f2dd233f3f4bc4c126ba6e195ff464d88
Luyanda Kwanele Kubheka <230120091@ump.ac.za>
2026-05-11 17:32:31 +0200

    feat: history feature progress

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import sessionRoutes from "./routes/session.routes";
 import signupRoutes from "./routes/signup.routes";
 import meRoutes from "./routes/me.routes";
 import alertRoutes from "./routes/alert.routes";
+import historyRoutes from "./routes/history.routes";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(signupRoutes);
 app.use(meRoutes);
 app.use("/api", alertRoutes); // Add alert routes
 app.use("/sessions", sessionRoutes);
+app.use("/history", historyRoutes);
 
 // Test route
 
